7af3eb3655c73e92156ac267226978e548cf2b5a
Lyana <shabbyacc@outlook.com>
2025-02-21 21:22:25 +0800

    update

--- preferences/powershell/Microsoft.PowerShell_profile.ps1
+++ preferences/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,5 @@
 $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = [Text.Encoding]::UTF8
 
+fnm env --use-on-cd --corepack-enabled --shell powershell | Out-String | Invoke-Expression
+
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/space.omp.json" | Invoke-Expression
