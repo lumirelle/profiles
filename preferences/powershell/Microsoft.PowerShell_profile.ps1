82bc9f73ee84ac6107ddf8d072cfc75b1f48f227
Lyana <shabbyacc@outlook.com>
2025-03-18 11:05:10 +0800

    feat: update

--- preferences/powershell/Microsoft.PowerShell_profile.ps1
+++ preferences/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,8 @@
 $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = [Text.Encoding]::UTF8
 
+Remove-Item Alias:ni -Force -ErrorAction Ignore
+New-Alias -Name touch -Value New-Item
+
 fnm env --use-on-cd --corepack-enabled --shell powershell | Out-String | Invoke-Expression
 
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/the-unnamed.omp.json" | Invoke-Expression
