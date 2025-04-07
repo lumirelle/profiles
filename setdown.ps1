152e10ed803c86e0a7e800674309db8961c15d6a
Lyana <chenbin.tang@xiaoxitech.com>
2025-04-07 17:57:22 +0800

    feat: update

--- setdown.ps1
+++ setdown.ps1
@@ -1,12 +1,6 @@
 <#
 .DESCRIPTION
-  Quick setup profiles
-
-.PARAMETER IgnoreFiles
-  The files you want to ignore, separated by comma
-
-.PARAMETER Override
-  Force to override the existing files
+  Quick set down profiles
 
 .PARAMETER Purpose
   The purpose of the profiles, expected values: `self`, `work`
@@ -14,26 +8,12 @@
   It is designed for my self, because no one will upload his working profile to github.
 
 .EXAMPLE
-  .\scripts\setup.ps1
+  .\scripts\setdown.ps1
 
-  This will create the symbolic links of supported profiles to your computer, so that you can receive the updates automatically.
-
-.EXAMPLE
-  .\scripts\setup.ps1 -IgnoreFiles ".gitconfig,.npmrc,.zshrc"
-
-  If you want to ignore some files, you can add them to the `IgnoreFiles` parameter.
-
-  This will ignore `.gitconfig`, `.npmrc` and `.zshrc`.
+  This will remove the symbolic links of supported profiles to your computer.
 
 .EXAMPLE
-  .\scripts\setup.ps1 -Force
-
-  If you want to force override the existing files, you can add the `Override` parameter.
-
-  This will force override the existing files.
-
-.EXAMPLE
-  .\scripts\setup.ps1 -Purpose "work"
+  .\scripts\setdown.ps1 -Purpose "work"
 
   If you want to use a specific purpose, you can add the `Purpose` parameter.
 
@@ -42,22 +22,12 @@
 
 param (
   [Parameter(Mandatory = $false)]
-  [Alias("i")]
-  [string[]]$IgnoreFiles = @(),
-
-  [Parameter(Mandatory = $false)]
-  [Alias("o")]
-  [switch]$Override,
-
-  [Parameter(Mandatory = $false)]
   [Alias("p")]
   [ValidateSet('self', 'work')]
   [string]$Purpose = 'self'
 )
 
 Write-Debug "Parameters:"
-Write-Debug "IgnoreFiles = $IgnoreFiles"
-Write-Debug "Override = $Override"
 Write-Debug "Purpose = $Purpose`n"
 
 # == INIT & CHECK ==
@@ -76,15 +46,12 @@ $PROFILE_FOLDERS = @(
 )
 
 # 忽略文件（增加内置）
-$IgnoreFiles += @(
-  'README.md'
-)
 
 # 根目录路径
 $RootDirPath = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
 
 # == SETUP ==
-Write-Host "`nStart to setup profiles...`n" -ForegroundColor Green
+Write-Host "`nStart to setdown profiles...`n" -ForegroundColor Green
 
 # 创建符号链接
 foreach ($folder in $PROFILE_FOLDERS) {
@@ -97,37 +64,30 @@ foreach ($folder in $PROFILE_FOLDERS) {
 
   $files = Get-ChildItem -Path $ProfilesDirPath -Recurse -File
   foreach ($file in $files) {
-    # 如果 $file.Name 在 $IgnoreFiles 中，则跳过
-    if ($IgnoreFiles -contains $file.Name) {
-      Write-Host "Ignore files: $($file.Name)`n" -ForegroundColor Cyan
-      continue
-    }
-
     # 计算目标路径
     # $RelativePath 是具体配置文件相对 $ProfilesDirPath 的路径
     $RelativePath = $file.FullName.Substring($ProfilesDirPath.Length + 1)
     $TargetFilePath = Join-Path $folder.Path $RelativePath
     $TargetDirPath = Split-Path -Path $TargetFilePath -Parent
 
-    # 如果目标目录 $TargetDirPath 不存在，则创建
+    # 如果目标目录 $TargetDirPath 不存在，则跳过
     if (-not (Test-Path $TargetDirPath)) {
-      New-Item -ItemType Directory -Path $TargetDirPath -Force > $null
+      continue
     }
 
     # 如果目标文件已存在
     if (Test-Path $TargetFilePath) {
-      if ($Override) {
-        Write-Host "File already exists: $RelativePath, will force override" -ForegroundColor Yellow
+      # 如果目标文件是符号链接，则删除
+      if ((Get-Item $TargetFilePath).LinkType -eq 'SymbolicLink') {
+        Remove-Item -Path $TargetFilePath -Force:$Force > $null
+        Write-Host "Removed symbolic link: $TargetFilePath`n"
       }
+      # 如果目标文件不是符号链接，则提示
       else {
-        Write-Host "File already exists: $RelativePath, will skip`n" -ForegroundColor Yellow
+        Write-Host "Target file are not symbolic link: $TargetFilePath, skip`n" -ForegroundColor Yellow
         continue
       }
     }
-
-    # 创建符号链接
-    New-Item -ItemType SymbolicLink -Path $TargetFilePath -Target $file.FullName -Force:$Override > $null
-    Write-Host "Created symbolic link: $TargetFilePath -> $($file.FullName)`n"
   }
 }
 
