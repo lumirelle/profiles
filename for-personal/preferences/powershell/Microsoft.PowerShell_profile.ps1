8088e2159601885b6c12ab90221975e4a360fd86
Lumirelle <shabbyacc@outlook.com>
2025-04-24 23:54:14 +0800

    feat: add pwsh alias

--- for-personal/preferences/powershell/Microsoft.PowerShell_profile.ps1
+++ for-personal/preferences/powershell/Microsoft.PowerShell_profile.ps1
@@ -2,8 +2,17 @@
 $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = [Text.Encoding]::UTF8
 
 # Alias
+# -- touch
 Remove-Item Alias:ni -Force -ErrorAction Ignore
 New-Alias -Name touch -Value New-Item
+# -- clr
+New-Alias -Name clr -Value Clear-Host
+# -- chs
+function Clear-AllHistory {
+  [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
+}
+New-Alias -Name chs -Value Clear-AllHistory
+
 
 # Environment
 fnm env --use-on-cd --corepack-enabled --shell powershell | Out-String | Invoke-Expression
