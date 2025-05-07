d36291f5c650e9b1d783fa451b84751795ab526c
Lumirelle <shabbyacc@outlook.com>
2025-05-07 11:20:44 +0800

    chore: update pwsh profile

--- resources/personal/preferences/terminal/powershell/Microsoft.PowerShell_profile.ps1
+++ resources/personal/preferences/terminal/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,18 +1,21 @@
 # Encoding
 $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = [Text.Encoding]::UTF8
 
-# Environment Variables
+# Variables
 # -- $env:PATH
 # Add current node_modules/.bin to PATH if it exists, so you can run npm scripts without `npx`
 $sep = [System.IO.Path]::DirectorySeparatorChar
 if (Test-Path -Path "node_modules") {
   $env:PATH = "{0}{1}node_modules{1}.bin;{2}" -f (Get-Location), $sep, $env:PATH
 }
-# -- $env:HISTORY
-$history = Join-Path $env:APPDATA "Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
-if (Test-Path $history) {
-  $env:HISTORY = $history
-}
+# -- $HISTORY
+$HISTORY = Join-Path $env:APPDATA "Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
+
+# Dev Environment
+fnm env --use-on-cd --corepack-enabled --shell powershell | Out-String | Invoke-Expression
+
+# UI
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/the-unnamed.omp.json" | Invoke-Expression
 
 # Command Aliases
 # -- touch
@@ -23,16 +26,8 @@ New-Alias -Name grep -Value Select-String
 # -- clh
 function Clear-AllHistory {
   [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
-  $historyPath = Join-Path $env:APPDATA "Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
-  if (Test-Path $historyPath) {
-    Clear-Content $historyPath -Force
+  if (Test-Path $HISTORY) {
+    Clear-Content $HISTORY -Force
   }
 }
 New-Alias -Name clh -Value Clear-AllHistory
-
-
-# Dev Environment
-fnm env --use-on-cd --corepack-enabled --shell powershell | Out-String | Invoke-Expression
-
-# UI
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/the-unnamed.omp.json" | Invoke-Expression
