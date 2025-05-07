b16148f17f9e3b4307b6a41682a848da1f2006aa
Lumirelle <shabbyacc@outlook.com>
2025-05-07 11:17:20 +0800

    feat: add $env:HISTORY for quickly finding of pwsh history file

--- resources/personal/preferences/terminal/powershell/Microsoft.PowerShell_profile.ps1
+++ resources/personal/preferences/terminal/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,20 @@
 # Encoding
 $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = [Text.Encoding]::UTF8
 
-# Path
-# -- Add current node_modules/.bin to PATH if it exists, so you can run npm scripts without `npx`
+# Environment Variables
+# -- $env:PATH
+# Add current node_modules/.bin to PATH if it exists, so you can run npm scripts without `npx`
 $sep = [System.IO.Path]::DirectorySeparatorChar
 if (Test-Path -Path "node_modules") {
   $env:PATH = "{0}{1}node_modules{1}.bin;{2}" -f (Get-Location), $sep, $env:PATH
 }
+# -- $env:HISTORY
+$history = Join-Path $env:APPDATA "Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
+if (Test-Path $history) {
+  $env:HISTORY = $history
+}
 
-# Alias
+# Command Aliases
 # -- touch
 Remove-Item Alias:ni -Force -ErrorAction Ignore
 New-Alias -Name touch -Value New-Item
@@ -17,11 +23,15 @@ New-Alias -Name grep -Value Select-String
 # -- clh
 function Clear-AllHistory {
   [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
+  $historyPath = Join-Path $env:APPDATA "Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
+  if (Test-Path $historyPath) {
+    Clear-Content $historyPath -Force
+  }
 }
 New-Alias -Name clh -Value Clear-AllHistory
 
 
-# Environment
+# Dev Environment
 fnm env --use-on-cd --corepack-enabled --shell powershell | Out-String | Invoke-Expression
 
 # UI
